5bc73a1105d3a0f2024e9d40e4d954803033ea5d
Edward Evangelista <ted11697@gmail.com>
2019-09-13 17:26:07 -0400

    slight contact changes

--- mern-project1-cm/config/keys.js
+++ mern-project1-cm/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb+srv://ted-project-admin:Pm2b6mS4D6@cluster0-imztr.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://ted-project-admin:12345@cluster0-imztr.mongodb.net/test?retryWrites=true&w=majority",
     secretOrKey: "GoodSecret"
 };
 
