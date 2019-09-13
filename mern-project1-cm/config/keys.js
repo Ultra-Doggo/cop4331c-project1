de56dbf5f2f3b079b738bb62173f767bb168fe6b
Edward Evangelista <ted11697@gmail.com>
2019-09-13 12:51:32 -0400

    file cleanups

--- mern-project1-cm/config/keys.js
+++ mern-project1-cm/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb+srv://ted-project-admin:2hoZPj8cB3@cluster0-imztr.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://ted-project-admin:123456789@cluster0-imztr.mongodb.net/test?retryWrites=true&w=majority",
     secretOrKey: "GoodSecret"
 };
 
